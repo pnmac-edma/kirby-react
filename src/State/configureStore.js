49514a88cc0dc5b3216bbb3b89dc7733af86e6dc
Abby Bashore <abby.bashore@pnmac.com>
2019-11-20 14:50:09 -0700

    small folder restructuring
     # now actually added the files lol

--- src/State/configureStore.js
+++ src/State/configureStore.js
@@ -1,7 +1,7 @@
 // import * as reducers from './Ducks/index';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { rootSaga, rootReducer } from './Ducks/index';
+import { rootSaga, rootReducer } from '/.index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
