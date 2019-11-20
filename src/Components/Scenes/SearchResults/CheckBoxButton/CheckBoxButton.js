2bd69dc53b7b068a38afa23312cf0279d931b590
Selcuk Ates <selcuk.ates@pnmac.com>
2019-11-20 13:08:35 -0800

    removed the logic from jsx

--- src/Components/Scenes/SearchResults/CheckBoxButton/CheckBoxButton.js
+++ src/Components/Scenes/SearchResults/CheckBoxButton/CheckBoxButton.js
@@ -19,25 +19,28 @@ const CheckBoxButton = props => {
   const classes = useStyles();
   const { searchResultCopy, requestAssetsClick } = props;
   const searchResultChecked = searchResultCopy.filter(obj => obj.checked);
+  const searchResultLenght = searchResultChecked.length;
+  const buttonText =
+    searchResultLenght === 1
+      ? `Request ${searchResultLenght} Asset`
+      : searchResultLenght > 1
+      ? `Request ${searchResultLenght} Assets`
+      : 'Request Asset';
 
   return (
-    <React.Fragment>
+    <>
       <Link to="/RequestAsset" className={classes.underline}>
         <Button
           color="primary"
           variant="contained"
-          disabled={searchResultChecked.length <= 0}
+          disabled={searchResultLenght <= 0}
           className={classes.button}
           onClick={() => requestAssetsClick(searchResultChecked)}
         >
-          {searchResultChecked.length === 1
-            ? `Request ${searchResultChecked.length} Asset`
-            : searchResultChecked.length > 1
-            ? `Request ${searchResultChecked.length} Assets`
-            : 'Request Asset'}
+          {buttonText}
         </Button>
       </Link>
-    </React.Fragment>
+    </>
   );
 };
 
