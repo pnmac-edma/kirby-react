ef8ea61ff42a5693883636d6177b2deea94484ae
Selcuk Ates <selcuk.ates@pnmac.com>
2019-11-20 11:48:13 -0800

    fixed component error

--- src/Components/Scenes/RequestAssets/TableHeadSection/TableHeadSection.js
+++ src/Components/Scenes/RequestAssets/TableHeadSection/TableHeadSection.js
@@ -1,33 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  TableCell,
-  TableRow,
-  TableHead,
-  Checkbox,
-  Typography,
-  Toolbar
-} from '@material-ui/core';
-
-const useToolbarStyles = makeStyles(theme => ({
-  title: {
-    flex: '0 0 auto'
-  }
-}));
+import { TableCell, TableRow, TableHead, Checkbox } from '@material-ui/core';
 
 const TableHeadSection = props => {
   const { selectedAll, requestCheckBoxSelect } = props;
-  const classes = useToolbarStyles();
+
   return (
     <React.Fragment>
       <TableHead>
-        <Toolbar>
-          <div className={classes.title}>
-            <Typography variant="h6" id="tableTitle">
-              Assets in this request
-            </Typography>
-          </div>
-        </Toolbar>
         <TableRow>
           <TableCell padding="checkbox" onClick={e => requestCheckBoxSelect(e)}>
             <Checkbox id="all" checked={selectedAll} />
