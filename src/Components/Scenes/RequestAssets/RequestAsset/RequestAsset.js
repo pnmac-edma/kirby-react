a20e5d8af13592099e57fe12a48f3ba23da73dc0
Selcuk Ates <selcuk.ates@pnmac.com>
2019-11-20 07:44:18 -0800

    working on the css;

--- src/Components/Scenes/RequestAssets/RequestAsset/RequestAsset.js
+++ src/Components/Scenes/RequestAssets/RequestAsset/RequestAsset.js
@@ -4,30 +4,50 @@ import RequestAssetJustificationContainer from '../RequestAssetJustification/Req
 import RequestedForContainer from '../RequestedFor/RequestedFor-Container';
 import AppBarContainer from '../../Chrome/AppBar/AppBar-Container';
 import { makeStyles } from '@material-ui/core/styles';
+import color from '@edma/design-tokens/js/color';
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap'
   },
   sidebar: {
-    position: 'fixed',
-    width: '31.21%',
+    // position: 'fixed',
+    // width: '31.21%',
+    // height: '100vh',
+    // background: 'rgba(0, 0, 0, 0.09)',
+    // fontSize: '0.65em'
+    // position: 'absolute',
+    // display: 'flex',
+    // flexDirection: 'column',
+    // backgroundColor: 'rgba(0, 0, 0, 0.09)',
+    // width: '31.21%',
+    // height: '100vh',
+    // color: color.white
+  },
+  pageContainer: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: theme.palette.type === 'light' ? 'dark' : 'light',
+    width: '100%',
     height: '100vh',
-    background: 'rgba(0, 0, 0, 0.09)',
-    fontSize: '0.65em'
+    color: color.white
   }
 }));
 
 const RequestAsset = props => {
   const classes = useStyles();
   return (
-    <div className={classes.sidebar}>
-      <AppBarContainer />
-      <RequestedForContainer />
-
-      <RequestAssetTableContainer />
-      <RequestAssetJustificationContainer />
-    </div>
+    <>
+      <div className={classes.pageContainer}>
+        <AppBarContainer />
+        <div className={classes.sidebar}>
+          <RequestedForContainer />
+          <RequestAssetTableContainer />
+          <RequestAssetJustificationContainer />
+        </div>
+      </div>
+    </>
   );
 };
 export default RequestAsset;
