3e362503df0ae193a0263a162df7b35ef284e283
Selcuk Ates <selcuk.ates@pnmac.com>
2019-11-25 12:56:06 -0800

    changed variable name in api call and added desing token

--- src/Saga/requestAsset.js
+++ src/Saga/requestAsset.js
@@ -1,10 +1,10 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
 import * as types from './../Actions/types';
-import { requestAssetApiCall } from '../Api/requestAsset';
+import { getEmployees } from '../Api/requestAsset';
 
 function* requestAsset() {
   try {
-    const response = yield call(requestAssetApiCall);
+    const response = yield call(getEmployees);
     yield put({ type: types.GET_EMPLOYEES_SUCCESS, payload: response });
   } catch (error) {
     yield put({ type: types.GET_EMPLOYEES_FAILURE, payload: error });
