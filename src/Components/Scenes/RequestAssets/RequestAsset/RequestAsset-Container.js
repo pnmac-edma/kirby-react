c0dc7217825caa3a59bc8d4914a4dcb14aaa53f9
Selcuk Ates <selcuk.ates@pnmac.com>
2019-11-25 11:20:38 -0800

    changed the name requestasset

--- src/Components/Scenes/RequestAssets/RequestAsset/RequestAsset-Container.js
+++ src/Components/Scenes/RequestAssets/RequestAsset/RequestAsset-Container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import RequestTable from './RequestAsset';
+import RequestAsset from './RequestAsset';
 import { getEmployeesFetch } from '../../../../Actions/requestAssetActions';
 
 const mapStateToProps = state => {
@@ -14,4 +14,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(RequestTable);
+export default connect(mapStateToProps, mapDispatchToProps)(RequestAsset);
