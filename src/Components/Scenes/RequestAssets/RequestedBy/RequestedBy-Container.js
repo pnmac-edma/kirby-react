0f8b173e2789d44099b2aba46eb665fad447ded3
Selcuk Ates <selcuk.ates@pnmac.com>
2019-12-04 14:07:53 -0800

    added comment

--- src/Components/Scenes/RequestAssets/RequestedBy/RequestedBy-Container.js
+++ src/Components/Scenes/RequestAssets/RequestedBy/RequestedBy-Container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import RequestedBy from './RequestedBy';
 
+// This will get updated when Auth is done
 const mapStateToProps = ({ requestAssets }) => {
   return {
     requestedBy: `Scott Fowles`
