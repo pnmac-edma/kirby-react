828e61c8ef9653e2819850cf57e13575b10d3778
Libbey Held <libbey.held@pnmac.com>
2019-12-04 16:21:44 -0700

    Add drag & drop functionality

--- src/Components/Scenes/Hydration/NewJob/Canvas.js
+++ src/Components/Scenes/Hydration/NewJob/Canvas.js
@@ -1,42 +1,180 @@
-import {
-  DiagramEngine,
-  DiagramModel,
-  DefaultNodeModel,
-  LinkModel,
-  DiagramWidget,
-  DefaultLinkModel
-} from '@projectstorm/react-diagrams';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import * as _ from 'lodash';
+import * as SRD from '@projectstorm/react-diagrams';
 require('@projectstorm/react-diagrams/dist/style.min.css');
 
-export default () => {
-  //1) setup the diagram engine
-  var engine = new DiagramEngine();
-  engine.installDefaultFactories();
+class Application {
+  constructor() {
+    this.diagramEngine = new SRD.DiagramEngine();
+    this.diagramEngine.installDefaultFactories();
 
-  //2) setup the diagram model
-  var model = new DiagramModel();
+    this.newModel();
+  }
 
-  //3-A) create a default node
-  var node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-  let port1 = node1.addOutPort('Out');
-  node1.setPosition(100, 100);
+  newModel() {
+    this.activeModel = new SRD.DiagramModel();
+    this.diagramEngine.setDiagramModel(this.activeModel);
 
-  //3-B) create another default node
-  var node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
-  let port2 = node2.addInPort('In');
-  node2.setPosition(400, 100);
+    //3-A) create a default node
+    var node1 = new SRD.DefaultNodeModel('Node 1', 'rgb(0,192,255)');
+    let port = node1.addOutPort('Out');
+    node1.setPosition(100, 100);
 
-  // link the ports
-  let link1 = port1.link(port2);
-  link1.addLabel('Hello World!');
+    //3-B) create another default node
+    var node2 = new SRD.DefaultNodeModel('Node 2', 'rgb(192,255,0)');
+    let port2 = node2.addInPort('In');
+    node2.setPosition(400, 100);
 
-  //4) add the models to the root graph
-  model.addAll(node1, node2, link1);
+    // link the ports
+    let link1 = port.link(port2);
 
-  //5) load model into engine
-  engine.setDiagramModel(model);
+    this.activeModel.addAll(node1, node2, link1);
+  }
 
-  //6) render the diagram!
-  return <DiagramWidget className="srd-demo-canvas" diagramEngine={engine} />;
+  getActiveDiagram() {
+    return this.activeModel;
+  }
+
+  getDiagramEngine() {
+    return this.diagramEngine;
+  }
+}
+
+const TrayItemWidget = props => {
+  const { model, color, name, onClick } = props;
+
+  return (
+    <div
+      style={{ borderColor: color }}
+      draggable={true}
+      onDragStart={event => {
+        event.dataTransfer.setData('storm-diagram-node', JSON.stringify(model));
+      }}
+      className="tray-item"
+      onClick={onClick}
+    >
+      {name}
+    </div>
+  );
 };
+
+const TrayWidget = props => {
+  const { children } = props;
+  return <div className="tray">{children}</div>;
+};
+
+const BodyWidget = props => {
+  const { app } = props;
+
+  const createNode = type => {
+    var node = null;
+    var nodesCount = _.keys(
+      app
+        .getDiagramEngine()
+        .getDiagramModel()
+        .getNodes()
+    ).length;
+
+    if (type === 'in') {
+      node = new SRD.DefaultNodeModel(
+        'Node ' + (nodesCount + 1),
+        'rgb(192,255,0)'
+      );
+      node.addInPort('In');
+    } else if (type === 'out') {
+      node = new SRD.DefaultNodeModel(
+        'Node ' + (nodesCount + 1),
+        'rgb(0,192,255)'
+      );
+      node.addOutPort('Out');
+    }
+    node.x = 400;
+    node.y = 400;
+    app
+      .getDiagramEngine()
+      .getDiagramModel()
+      .addNode(node);
+    forceUpdate();
+  };
+
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
+  return (
+    <div className="body">
+      <div className="content">
+        <div
+          className="diagram-layer"
+          onDrop={event => {
+            var data = JSON.parse(
+              event.dataTransfer.getData('storm-diagram-node')
+            );
+            var nodesCount = _.keys(
+              app
+                .getDiagramEngine()
+                .getDiagramModel()
+                .getNodes()
+            ).length;
+
+            var node = null;
+            if (data.type === 'in') {
+              node = new SRD.DefaultNodeModel(
+                'Node ' + (nodesCount + 1),
+                'rgb(192,255,0)'
+              );
+              node.addInPort('In');
+            } else {
+              node = new SRD.DefaultNodeModel(
+                'Node ' + (nodesCount + 1),
+                'rgb(0,192,255)'
+              );
+              node.addOutPort('Out');
+            }
+            var points = app.getDiagramEngine().getRelativeMousePoint(event);
+            node.x = points.x;
+            node.y = points.y;
+            app
+              .getDiagramEngine()
+              .getDiagramModel()
+              .addNode(node);
+            forceUpdate();
+          }}
+          onDragOver={event => {
+            event.preventDefault();
+          }}
+        >
+          <SRD.DiagramWidget
+            className="srd-demo-canvas"
+            diagramEngine={app.getDiagramEngine()}
+          />
+        </div>
+        <TrayWidget>
+          <TrayItemWidget
+            model={{ type: 'out' }}
+            name="Out Node"
+            color="rgb(0,192,255)"
+            onClick={() => {
+              createNode('out');
+            }}
+          />
+          <TrayItemWidget
+            model={{ type: 'in' }}
+            name="In Node"
+            color="rgb(192,255,0)"
+            onClick={() => {
+              createNode('in');
+            }}
+          />
+        </TrayWidget>
+      </div>
+    </div>
+  );
+};
+
+const Canvas = () => {
+  var app = new Application();
+
+  return <BodyWidget app={app} />;
+};
+
+export default Canvas;
