a71ec374064d273a8bca23f4e0c4786e1a790e6e
Jake Lindvall <jake.lindvall@pnmac.com>
2019-12-09 16:02:25 -0700

    much better logic and flow for redirecting, auth, and 'restricting' the view of content

--- src/Components/Scenes/Chrome/PageWrapper/PageWrapper.js
+++ src/Components/Scenes/Chrome/PageWrapper/PageWrapper.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, useLocation } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
 import { makeStyles } from '@material-ui/core/styles';
@@ -29,29 +29,36 @@ const pageContainerStyle = makeStyles(theme => ({
 
 const PageWrapper = props => {
   const { isSearchClicked, sessionToken, error, authenticateFetch } = props;
-
   const classes = pageContainerStyle();
   const samlResponse = useQuery('SAMLResponse');
   const curPath = useLocation().pathname;
 
+  const [isRedirecting, setIsRedirecting] = useState(true);
+  // This functionality could be useful if wrapped in an error view button or link,
+  // so in anticipation, it's set up as a function for now
+  const redirect = () => {
+    setIsRedirecting(true);
+    window.location.replace('https://pennymac.onelogin.com/portal/');
+  };
+
   // Case 1: there is a SAML response but no session token, so authenticate real quick
-  // Case 2: (not seen here) there may or may not be a SAML response,
+  // Case 2: there is neither a SAML response nor a session token, so redirect to OneLogin
+  // Case 3: there may or may not be a SAML response,
   //         but there is a session token, so relax until an hour later
   //         when we get a 4xx code from some request, then redirect
-  // Case 3: there is neither a SAML response nor a session token, so redirect to OneLogin
   useEffect(() => {
     if (samlResponse && !sessionToken) {
+      setIsRedirecting(false);
       authenticateFetch(samlResponse);
     } else if (!sessionToken) {
-      window.location.replace('https://pennymac.onelogin.com/portal/');
+      redirect();
+    } else {
+      setIsRedirecting(false);
     }
   }, [samlResponse, authenticateFetch, sessionToken]);
 
-  return (
-    <div className={classes.pageContainer}>
-      {isEmptyObject(error) ? null : (
-        <span>OH NO AN AUTH ERROR! haha damn that sucks</span>
-      )}
+  const authenticatedContent = (
+    <>
       {curPath === '/hydration/new-job' ? null : <AppBarContainer />}
 
       <AnimatedSwitch
@@ -60,9 +67,9 @@ const PageWrapper = props => {
         atActive={{ opacity: 1 }}
         className="switch-wrapper"
       >
-        <Route exact path="/" component={Splash} />
-        <Route exact path="/index.html" component={Splash} />{' '}
         {/* hosted version lands users at index.html */}
+        <Route exact path="/index.html" component={Splash} />
+        <Route exact path="/" component={Splash} />
         {/* search pages */}
         <Route path="/search/access" component={RequestAssetContainer} />
         <Route path="/search" component={SearchResultsContainer} />
@@ -75,6 +82,19 @@ const PageWrapper = props => {
       </AnimatedSwitch>
 
       {isSearchClicked ? <SearchContainer /> : null}
+    </>
+  );
+
+  // TODO: replace <span> with proper error view once it has landed
+  return (
+    <div className={classes.pageContainer}>
+      {!isRedirecting ? (
+        isEmptyObject(error) ? (
+          authenticatedContent
+        ) : (
+          <span>auth error placeholder</span>
+        )
+      ) : null}
     </div>
   );
 };
