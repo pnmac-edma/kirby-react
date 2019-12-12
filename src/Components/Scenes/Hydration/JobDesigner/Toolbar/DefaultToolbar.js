a5a2aa261e9d952ae398805dd3e0b41aee66e097
Libbey Held <libbey.held@pnmac.com>
2019-12-12 14:11:11 -0700

    Match default toolbar config to UI mocks

---
+++ src/Components/Scenes/Hydration/JobDesigner/Toolbar/DefaultToolbar.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { Box, Divider, List, ListItem, Tab, Tabs } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import UndoIcon from '@material-ui/icons/Undo';
+import RedoIcon from '@material-ui/icons/Redo';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import color from '@edma/design-tokens/js/color';
+
+const ToolbarItemWidget = props => {
+  const { model, color, name, onClick } = props;
+
+  return (
+    <Box style={{ display: 'flex', alignItems: 'center' }}>
+      <div
+        style={{ borderColor: color, height: '20px', width: '30px' }}
+        draggable={true}
+        onDragStart={event => {
+          event.dataTransfer.setData(
+            'storm-diagram-node',
+            JSON.stringify(model)
+          );
+        }}
+        className="toolbar-item"
+        onClick={onClick}
+      ></div>
+      {name}
+    </Box>
+  );
+};
+
+const SourcesList = props => {
+  const { addNodeToDiagram } = props;
+  return (
+    <List>
+      <ListItem>
+        <ToolbarItemWidget
+          model={{ type: 'source' }}
+          name="Source Node"
+          color={color['c400']}
+          onClick={() => addNodeToDiagram('source', 400, 400)}
+        />
+      </ListItem>
+    </List>
+  );
+};
+
+const TransformsList = props => {
+  const { addNodeToDiagram } = props;
+  return (
+    <List>
+      <ListItem>
+        <ToolbarItemWidget
+          model={{ type: 'trans' }}
+          name="Transformation Node"
+          color={color['v400']}
+          onClick={() => addNodeToDiagram('trans', 400, 400)}
+        />
+      </ListItem>
+    </List>
+  );
+};
+
+const DestinationsList = props => {
+  const { addNodeToDiagram } = props;
+  return (
+    <List>
+      <ListItem>
+        <ToolbarItemWidget
+          model={{ type: 'dest' }}
+          name="Destination Node"
+          color={color['r300']}
+          onClick={() => addNodeToDiagram('dest', 400, 400)}
+        />
+      </ListItem>
+    </List>
+  );
+};
+
+export const DefaultToolbar = props => {
+  const { addNodeToDiagram } = props;
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const tabNames = ['Sources', 'Transforms', 'Destinations'];
+  const componentMap = {
+    Sources: <SourcesList addNodeToDiagram={addNodeToDiagram} />,
+    Transforms: <TransformsList addNodeToDiagram={addNodeToDiagram} />,
+    Destinations: <DestinationsList addNodeToDiagram={addNodeToDiagram} />
+  };
+
+  const renderToolbarBody = () => {
+    const selectedTabName = tabNames[selectedTab];
+    return <Box>{componentMap[selectedTabName]}</Box>;
+  };
+
+  const tabStyles = makeStyles(theme => ({
+    tabIndicator: {
+      backgroundColor: 'transparent'
+    },
+    sourcesSelected: {
+      color: color['c400']
+    },
+    transSelected: {
+      color: color['v400']
+    },
+    destSelected: {
+      color: color['r300']
+    }
+  }));
+  const classes = tabStyles();
+  return (
+    <Box>
+      <Box>
+        <UndoIcon />
+        <RedoIcon />
+        <PlayArrowIcon />
+      </Box>
+      <Divider />
+      <Tabs
+        classes={{ indicator: classes.tabIndicator }}
+        value={selectedTab}
+        onChange={(e, newTabValue) => setSelectedTab(newTabValue)}
+        indicatorColor="primary"
+        centered
+      >
+        <Tab label="Sources" classes={{ selected: classes.sourcesSelected }} />
+        <Tab label="Transforms" classes={{ selected: classes.transSelected }} />
+        <Tab
+          label="Destinations"
+          classes={{ selected: classes.destSelected }}
+        />
+      </Tabs>
+      <Divider />
+      {renderToolbarBody()}
+      <Divider />
+    </Box>
+  );
+};
+
+export default DefaultToolbar;
--- src/Components/Scenes/Hydration/JobDesigner/Toolbar/Toolbar.js
+++ src/Components/Scenes/Hydration/JobDesigner/Toolbar/Toolbar.js
@@ -1,48 +1,9 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import color from '@edma/design-tokens/js/color';
-
-const toolbarStyles = makeStyles(theme => ({
-  typography: {
-    textAlign: 'center'
-  }
-}));
-
-const ToolbarItemWidget = props => {
-  const { model, color, name, onClick } = props;
-
-  return (
-    <div
-      style={{ borderColor: color }}
-      draggable={true}
-      onDragStart={event => {
-        event.dataTransfer.setData('storm-diagram-node', JSON.stringify(model));
-      }}
-      className="toolbar-item"
-      onClick={onClick}
-    >
-      {name}
-    </div>
-  );
-};
-
-const ToolbarWidget = props => {
-  const classes = toolbarStyles();
-  const { children } = props;
-  return (
-    <div className="toolbar">
-      <Typography variant="h5" className={classes.typography}>
-        Pick A Node, Any Node
-      </Typography>
-      {children}
-    </div>
-  );
-};
-
+import DefaultToolbar from './DefaultToolbar';
 export const Toolbar = props => {
-  const classes = toolbarStyles();
   const { selectedNode, addNodeToDiagram } = props;
+
   if (selectedNode !== null) {
     const toolbarType = {
       source: 'Source',
@@ -51,7 +12,7 @@ export const Toolbar = props => {
     }[selectedNode.type];
     return (
       <div className="toolbar">
-        <Typography className={classes.typography} variant="h5">
+        <Typography variant="h5">
           Toolbar for {toolbarType}
           <br /> Node ID:
           <br /> {selectedNode.id}
@@ -59,27 +20,8 @@ export const Toolbar = props => {
       </div>
     );
   } else {
-    return (
-      <ToolbarWidget>
-        <ToolbarItemWidget
-          model={{ type: 'source' }}
-          name="Source Node"
-          color={color['c400']}
-          onClick={() => addNodeToDiagram('source', 400, 400)}
-        />
-        <ToolbarItemWidget
-          model={{ type: 'trans' }}
-          name="Transformation Node"
-          color={color['v400']}
-          onClick={() => addNodeToDiagram('trans', 400, 400)}
-        />
-        <ToolbarItemWidget
-          model={{ type: 'dest' }}
-          name="Destination Node"
-          color={color['r300']}
-          onClick={() => addNodeToDiagram('dest', 400, 400)}
-        />
-      </ToolbarWidget>
-    );
+    return <DefaultToolbar addNodeToDiagram={addNodeToDiagram} />;
   }
 };
+
+export default Toolbar;
