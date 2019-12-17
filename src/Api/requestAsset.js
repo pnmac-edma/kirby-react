b4cd7be2b52f0cd261e0e7b9b7b1e37f5b8e3430
Abby Bashore <abby.bashore@pnmac.com>
2019-12-17 09:37:39 -0700

    revert API requests to non auth url

--- src/Api/requestAsset.js
+++ src/Api/requestAsset.js
@@ -2,10 +2,13 @@ import axios from 'axios';
 import { constructRequest } from '../State/helpers';
 
 export const getEmployees = args => {
-  // TODO: pull url from env config once implemented
-  const url = `https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/dev`;
+  // TODO: Pull base url from the env config object once implemented
+  const BASE_URL = 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/dev';
 
-  const request = constructRequest(url, 'GET', '/users', null, null);
+  // to test signing (once implemented), Jon created this url that requires signing to access
+  // const BASE_URL = 'https://vtc55uxuy5.execute-api.us-west-2.amazonaws.com/dev';
+
+  const request = constructRequest(BASE_URL, 'GET', '/users', null, null);
 
   return axios(request)
     .then(response => response.data.users)
--- src/Api/searchResult.js
+++ src/Api/searchResult.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 import { constructRequest } from '../State/helpers';
 
 export const searchResultApiCall = (...args) => {
-  // TODO: Pull url from env config once implemented
-  // const url = `https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/dev`;
+  // TODO: Pull base url from the env config object once implemented
+  const BASE_URL = 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/dev';
 
-  const url = 'https://vtc55uxuy5.execute-api.us-west-2.amazonaws.com/dev';
+  // to test signing (once implemented), Jon created this url that requires signing to access
+  // const BASE_URL = 'https://vtc55uxuy5.execute-api.us-west-2.amazonaws.com/dev';
   const searchTerm = args[0] || '';
   const filterQueries = args[2] ? undefined : args[1];
   const sortBy = args[2] ? [{ [args[1]]: args[2].payload }] : undefined;
@@ -18,7 +19,7 @@ export const searchResultApiCall = (...args) => {
   };
 
   const request = constructRequest(
-    url,
+    BASE_URL,
     'POST',
     '/assets/search',
     null,
