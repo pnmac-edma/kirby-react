f09cde0f528d9d126e5b477e519d8ce4c0bc4d87
Abby Bashore <57811488+abbyjb@users.noreply.github.com>
2019-12-17 11:00:31 -0700

    Reafactor API to construct request as object vs chaining [KIR-354] (#39)
    * finish helper method, start refactoring API calls
    
    * base http client done
    
    * refactor old API requests to the new URL; comments
    
    * test linter
    
    * test linter again
    
    * small fix
    
    * fix eslint error
    
    * cleaning code according to MR comments
    
    * attempt testing API that requires signing
    
    * regex hostname from url; blocker
    
    * fix merge due to circular warning
    
    * small changes
    
    * latest changes, still throwing InvalidSignatureException
    
    * take out signing
    
    * revert API requests to non auth url
    
    * more cleanup

--- src/Api/searchResult.js
+++ src/Api/searchResult.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
+import { constructRequest } from '../State/helpers';
 
 export const searchResultApiCall = (...args) => {
-  const url = `https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com/dev/assets`;
+  // TODO: Pull base url from the env config object once implemented
+  const BASE_URL = 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/dev';
+
+  // to test signing (once implemented), Jon created this url that requires signing to access
+  // const BASE_URL = 'https://vtc55uxuy5.execute-api.us-west-2.amazonaws.com/dev';
   const searchTerm = args[0] || '';
   const filterQueries = args[2] ? undefined : args[1];
   const sortBy = args[2] ? [{ [args[1]]: args[2].payload }] : undefined;
-  const fetchBody = {
-    function: 'search',
-    params: {
-      requestedbyemail: 'selcuk.ates@pnmac.com',
-      search_term: searchTerm,
-      sort_by: sortBy,
-      filters: filterQueries
-    }
+
+  const requestBody = {
+    requestedbyemail: 'selcuk.ates@pnmac.com',
+    search_term: searchTerm,
+    sort_by: sortBy,
+    filters: filterQueries
   };
-  return axios
-    .post(url, fetchBody)
+
+  const request = constructRequest(
+    BASE_URL,
+    'POST',
+    '/assets/search',
+    null,
+    requestBody
+  );
+
+  return axios(request)
     .then(response => response.data)
     .then(error => error);
 };
--- src/State/helpers.js
+++ src/State/helpers.js
@@ -1,4 +1,4 @@
-// ideally, would like to have methods that all ducks will need here
+import store from '../setupStore';
 
 // takes in requests data from api results and returns parseable data
 export const transformRequests = (requests, role) => {
@@ -26,23 +26,96 @@ export const transformRequests = (requests, role) => {
   });
 };
 
-// Attempts to load in the state from session storage if it's there
-// (https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage)
-export const loadState = () => {
-  try {
-    const sessionState = sessionStorage.getItem('kirbyState');
-    return sessionState !== null ? JSON.parse(sessionState) : undefined;
-  } catch (error) {
-    return undefined;
+/* constructs a request object to be passed to axios
+ * url: the base url of the API (pulled from the config in most ways)
+ * method: the HTTP method of the request
+ * path: the specific path to be appended to the host (e.g. /users/requests)
+ * params: any query paramters the request needs. Pass in null if there is no query string parameters
+ * data: the body of the request. Pass in null if there is no body
+ */
+export const constructRequest = (url, method, path, params, data) => {
+  const {
+    AccessKeyId,
+    EmpEmail,
+    SamlHash,
+    SecretKey,
+    SessionToken,
+    UserKey
+  } = store.getState().currentUser;
+
+  let userSession = {
+    AccessKeyId,
+    EmpEmail,
+    SamlHash,
+    SecretKey,
+    SessionToken,
+    UserKey
+  };
+
+  // regex to match on a hostname
+  // eslint-disable-next-line
+  let hostRegex = new RegExp(/([(\w(\-?)\.]+)com/g);
+
+  let hostname = hostRegex.exec(url)[0];
+  // construct request
+  let request = {
+    host: hostname,
+    method,
+    url: `${url}${path}`,
+    path,
+    data: { ...userSession }, // data paramter needed for axios
+    body: JSON.stringify({ ...userSession }) // body parameter needed for aws
+  };
+
+  console.log(request);
+
+  /* NOTE:
+   * Adding these headers violates CORS policies!
+   * For now, this will be commented out, but once
+   * we are able to get signing implemented correctly
+   * this can be implemented
+   */
+
+  // request.headers["AccessKeyId"] = AccessKeyId;
+  // request.headers["EmpEmail"] = EmpEmail;
+  // request.headers["SamlHash"] = SamlHash;
+  // request.headers["SecretKey"] = SecretKey;
+  // request.headers["SessionToken"] = SessionToken;
+  // request.headers["UserKey"] = UserKey;
+
+  // add params if needed
+  if (params) {
+    request.params = params;
   }
+
+  // tack on additional data if needed
+  if (data) {
+    request.data = { ...request.data, ...data };
+    request.body = JSON.stringify(request.data);
+  }
+
+  // TODO: Implement aws4 signing
+  // let signedRequest = signApiCall(request, {AccessKeyId, SecretKey, SessionToken});
+
+  return request;
 };
 
-// Persists the state to session storage to avoid losing it on refresh
-export const saveState = state => {
-  try {
-    const sessionState = JSON.stringify(state);
-    sessionStorage.setItem('kirbyState', sessionState);
-  } catch (error) {
-    // Prevents a crash, but state save failures are not critical to handle further
-  }
-};
+// TODO: implement signing. The aws4 package is not signing correctly
+// so signing will have to be done manually
+// const signApiCall = request = credentials => {
+//   const { AccessKeyId, SecretKey, SessionToken } = credentials;
+
+//   if (AccessKeyId && SecretKey && SessionToken) {
+//     aws4.sign(request, {
+//       accessKeyId: AccessKeyId,
+//       secretAccessKey: SecretKey,
+//       sessionToken: SessionToken
+//     });
+
+//     delete request.headers['Host'];
+//     delete request.headers['Content-Length'];
+//   }
+//   console.log(request);
+//
+//   return request;
+// };
