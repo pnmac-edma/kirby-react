77561d45182745dea91ec954f102214e65735272
sfowles <scott.fowles@pnmac.com>
2019-12-18 05:36:42 -0800

    Update hydration flow styles

--- src/Components/Scenes/Hydration/JobDesigner/Toolbar/Toolbar.js
+++ src/Components/Scenes/Hydration/JobDesigner/Toolbar/Toolbar.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import {
+  Button,
+  Typography,
+  TextField,
+  MenuItem,
+  IconButton,
+  Tab,
+  Tabs
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
+import UndoIcon from '@material-ui/icons/Undo';
+import RedoIcon from '@material-ui/icons/Redo';
+import PlayIcon from '@material-ui/icons/PlayArrow';
 import color from '@edma/design-tokens/js/color';
 
 const toolbarStyles = makeStyles(theme => ({
   typography: {
-    textAlign: 'center'
+    textAlign: 'left',
+    padding: '8px 16px 16px'
+  },
+  iconButton: {
+    color: color.black
+  },
+  playButton: {
+    color: color.black,
+    background: color.y400,
+    minWidth: 40,
+    minHeight: 40
+  },
+  toolbarTopRight: {
+    position: 'absolute',
+    right: 12,
+    top: 8
   }
 }));
 
@@ -32,11 +56,40 @@ const ToolbarItemWidget = props => {
 const ToolbarWidget = props => {
   const classes = toolbarStyles();
   const { children } = props;
+  const [tab, setTab] = React.useState(0);
+
+  const handleTabsChange = (event, newTab) => {
+    setTab(newTab);
+  };
+
   return (
     <div className="Toolbar">
-      <Typography variant="h5" className={classes.typography}>
-        Pick A Node, Any Node
-      </Typography>
+      <div class="Toolbar__top">
+        <IconButton disabled aria-label="Undo" className={classes.iconButton}>
+          <UndoIcon fontSize="small" />
+        </IconButton>
+        <IconButton disabled aria-label="Redo" className={classes.iconButton}>
+          <RedoIcon fontSize="small" />
+        </IconButton>
+        <span className={classes.toolbarTopRight}>
+          <Button disabled className={classes.playButton}>
+            <PlayIcon aria-label="Schedule Job" fontSize="small" />
+          </Button>
+        </span>
+      </div>
+      <div class="Toolbar__tabs">
+        <Tabs
+          value={tab}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleTabsChange}
+          aria-label="Tile types"
+        >
+          <Tab label="Sources" className="Toolbar__sources-tab" />
+          <Tab label="Transforms" className="Toolbar__transforms-tab" />
+          <Tab label="Destinations" className="Toolbar__destinations-tab" />
+        </Tabs>
+      </div>
       {children}
     </div>
   );
@@ -77,13 +130,13 @@ export const Toolbar = props => {
       dest: 'Destination'
     }[selectedNode.type];
     return (
-      <div className="Toolbar">
+      <ToolbarWidget>
         <Typography className={classes.typography} variant="h5">
           Toolbar for {toolbarType}
           <br /> Node ID:
           <br /> {selectedNode.id}
         </Typography>
-      </div>
+      </ToolbarWidget>
     );
   } else {
     return (
@@ -106,28 +159,30 @@ export const Toolbar = props => {
           color={color['r300']}
           onClick={() => addNodeToDiagram('dest', 400, 400)}
         />
-        <div>
-          <TextField
-            id="example"
-            label="Example TextField"
-            className="Toolbar__textfield"
-          />
-        </div>
-        <div>
-          <TextField
-            select
-            className="Toolbar__select"
-            id="select-example"
-            label="Example Select"
-            value={value}
-            onChange={handleSelectChange}
-          >
-            {options.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+        <div className={classes.typography}>
+          <div>
+            <TextField
+              id="example"
+              label="Example TextField"
+              className="Toolbar__textfield"
+            />
+          </div>
+          <div>
+            <TextField
+              select
+              className="Toolbar__select"
+              id="select-example"
+              label="Example Select"
+              value={value}
+              onChange={handleSelectChange}
+            >
+              {options.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
         </div>
       </ToolbarWidget>
     );
