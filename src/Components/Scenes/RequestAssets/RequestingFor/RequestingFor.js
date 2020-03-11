d1456ccde9be3f54f9b4f800c6d507d9480b7542
sfowles <sfowles@gmail.com>
2020-03-11 09:45:33 -0700

    Update indicatorSeparator for selectize menu in RequestingFor component

--- src/Components/Scenes/RequestAssets/RequestingFor/RequestingFor.js
+++ src/Components/Scenes/RequestAssets/RequestingFor/RequestingFor.js
@@ -19,27 +19,34 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-    fontSize: fontSize[0]
-  }),
-
-  control: (provided, state) => ({
-    ...provided,
-    border: 0,
-    borderBottom: '1px solid black',
-    borderRadius: 0,
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    fontSize: fontSize[0]
-  })
-};
-
 const RequestingFor = props => {
   const { employees, handleSelectedEmployees } = props;
   const classes = useStyles();
   const theme = useTheme();
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      fontSize: fontSize[0]
+    }),
+
+    control: (provided, state) => ({
+      ...provided,
+      border: 0,
+      borderBottom: `1px solid ${
+        theme.palette.type === 'light' ? color.black : color.white
+      }`,
+      borderRadius: 0,
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+      fontSize: fontSize[0]
+    }),
+
+    indicatorSeparator: (provided, state) => ({
+      ...provided,
+      display: 'none'
+    })
+  };
 
   return (
     <React.Fragment>
