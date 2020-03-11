2c1f1b3e92ce8ebca67502f35a315dd5dd1db338
Scott Fowles <sfowles@gmail.com>
2020-03-11 10:12:13 -0700

    Update flexbox styles for Request Assets screen (#65)
    * Update flexbox styles for Request Assets screen
    Update Skeleton component to include colspan prop
    
    * Update "Requesting For" label in Request Assets component
    
    * Update indicatorSeparator for selectize menu in RequestingFor component

--- src/Components/Scenes/RequestAssets/RequestingFor/RequestingFor.js
+++ src/Components/Scenes/RequestAssets/RequestingFor/RequestingFor.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import Select, { createFilter } from 'react-select';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import EmployeesList from './EmployeesList';
 import { Typography } from '@material-ui/core';
-import fontSize from '@edma/design-tokens/js/fontSize';
+import { color, font, fontSize } from '@edma/design-tokens';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   table: {
-    width: 400
+    width: '100%'
   },
   menuStyle: {
     width: 'max-content',
     textTransform: 'uppercase',
-    marginBottom: 8
+    marginBottom: 8,
+    fontWeight: 'bold',
+    fontFamily: font.body,
+    color: theme.palette.type === 'light' ? color.black : color.g300
   }
-});
-
-const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-    fontSize: fontSize[0]
-  }),
-
-  control: (provided, state) => ({
-    ...provided,
-    border: 0,
-    borderBottom: '1px solid black',
-    borderRadius: 0,
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    fontSize: fontSize[0]
-  })
-};
+}));
 
 const RequestingFor = props => {
   const { employees, handleSelectedEmployees } = props;
   const classes = useStyles();
+  const theme = useTheme();
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      fontSize: fontSize[0]
+    }),
+
+    control: (provided, state) => ({
+      ...provided,
+      border: 0,
+      borderBottom: `1px solid ${
+        theme.palette.type === 'light' ? color.black : color.white
+      }`,
+      borderRadius: 0,
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+      fontSize: fontSize[0]
+    }),
+
+    indicatorSeparator: (provided, state) => ({
+      ...provided,
+      display: 'none'
+    })
+  };
 
   return (
     <React.Fragment>
-      <Typography className={classes.menuStyle}>Requesting For</Typography>
+      <Typography variant="overline" className={classes.menuStyle}>
+        Requesting For
+      </Typography>
       <Select
         className={('basic-multi-select', classes.table)}
         filterOption={createFilter({ ignoreAccents: false })}
