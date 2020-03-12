ade555b86e2126fd65a4b915499c093d28c88d95
sfowles <sfowles@gmail.com>
2020-03-12 16:14:36 -0700

    Remove unused declaration

--- src/Components/Scenes/Hydration/App/Destination/Destination.tsx
+++ src/Components/Scenes/Hydration/App/Destination/Destination.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import { useFormikContext } from 'formik';
 import { color } from '@edma/design-tokens';
 import { InitialStateTypes } from '../../../../../State/Hydration/types';
