8aa0b08901b5da83589c1945ddb89a6d1aafa1d6
sfowles <sfowles@gmail.com>
2020-03-12 13:08:13 -0700

    Update Destination tile toolbar styles

--- src/Components/Scenes/Hydration/App/Destination/Destination.tsx
+++ src/Components/Scenes/Hydration/App/Destination/Destination.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
 import { useFormikContext } from 'formik';
+import { color } from '@edma/design-tokens';
 import { InitialStateTypes } from '../../../../../State/Hydration/types';
 
 const useStyles = makeStyles(theme => ({
-  typography: {
+  tileTitle: {
+    textDecoration: 'bold',
+    marginBottom: '1rem'
+  },
+  tileSection: {
+    position: 'relative',
     textAlign: 'left',
-    padding: '8px 16px 16px'
+    padding: '8px 16px 16px',
+    borderBottom: `1px solid ${
+      theme.palette.type === 'light' ? color.g100 : color.g700
+    }`
+  },
+  tileDescription: {
+    color: theme.palette.type === 'light' ? color.g600 : color.g400
   }
 }));
 
@@ -21,12 +33,10 @@ const Destination = (props: DestinationProps) => {
   const classes = useStyles();
 
   return (
-    <div>
-      <Typography className={classes.typography} variant="h4">
-        {destinations[id].name}
-      </Typography>
-      <p>{destinations[id].email}</p>
-      <p>{destinations[id].description}</p>
+    <div className={classes.tileSection}>
+      <h4 className={classes.tileTitle}>{destinations[id].name}</h4>
+      <p className={classes.tileDescription}>{destinations[id].email}</p>
+      <p className={classes.tileDescription}>{destinations[id].description}</p>
     </div>
   );
 };
