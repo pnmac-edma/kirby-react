864bbc68f77d593afab691f4b0eef1ea1c502cbf
sfowles <sfowles@gmail.com>
2020-03-13 12:46:50 -0700

    Job Scheduler styles first pass

--- src/Components/Scenes/Hydration/App/ScheduleJob/ScheduleJob.tsx
+++ src/Components/Scenes/Hydration/App/ScheduleJob/ScheduleJob.tsx
@@ -5,8 +5,11 @@ import {
   FormControlLabel,
   FormHelperText,
   Grid,
-  TextField
+  InputLabel,
+  TextField,
+  Typography
 } from '@material-ui/core';
+import { color, spacing } from '@edma/design-tokens';
 import { makeStyles } from '@material-ui/core/styles';
 import { Field, useFormikContext } from 'formik';
 import DateFnsUtils from '@date-io/date-fns';
@@ -24,7 +27,16 @@ import mockWeekOfMonth from '../../../../../State/__mockData__/mockWeekOfMonthDa
 
 const useStyles = makeStyles(theme => ({
   container: {
-    width: '500px' // TODO: change this
+    padding: spacing['3'],
+    background: color.g50
+  },
+  header: {
+    borderBottom: `1px solid ${
+      theme.palette.type === 'light' ? color.g100 : color.g700
+    }`
+  },
+  label: {
+    color: color.g600
   }
 }));
 
@@ -85,7 +97,7 @@ const ScheduleJob = (props: ScheduleJobProps) => {
 
   const repeatWeekSelect = (
     <SelectField
-      className=""
+      className="Scheduler__input"
       id="repeats-weekofmonth"
       name="scheduleJob.repeats.weekOfMonth"
       options={mockWeekOfMonth}
@@ -93,119 +105,176 @@ const ScheduleJob = (props: ScheduleJobProps) => {
   );
 
   return (
-    <div className={classes.container}>
-      <div>Schedule Job</div>
-      <SelectField
-        fullWidth
-        className=""
-        id="calendar-team"
-        label="Calendar"
-        name="scheduleJob.calendarTeam"
-        options={mockCalendarTeams}
-      />
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="starts-on-date"
-          label="Starts on"
-          value={scheduleJob.startsOn}
-          onChange={setDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date'
-          }}
-        />
-        <KeyboardTimePicker
-          margin="normal"
-          id="starts-on-time"
-          value={scheduleJob.startsOn}
-          onChange={setDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time'
-          }}
-        />
-      </MuiPickersUtilsProvider>
-      <div>
-        <Field
-          name="scheduleJob.repeats.num"
-          className=""
-          label="Repeat every"
-          type="number"
-          as={TextField}
-        />
-        <SelectField
-          className=""
-          id="repeats-interval"
-          name="scheduleJob.repeats.interval"
-          options={mockRepeatsData}
-        />
-        <Field
-          className=""
-          name="scheduleJob.fails.num"
-          label="Fails after"
-          type="number"
-          as={TextField}
-        />
-        <SelectField
-          className=""
-          id="fails-interval"
-          name="scheduleJob.fails.interval"
-          options={mockFailsData}
-        />
+    <>
+      <div className={`Scheduler ${classes.container} ${classes.header}`}>
+        <h2>Schedule Job</h2>
       </div>
-      {isRepeatWeeksOrMonths && (
-        <div>
-          {isRepeatWeeks ? (
-            <FormHelperText>Repeat on</FormHelperText>
-          ) : (
-            <>
-              <FormHelperText>Repeat on the</FormHelperText>
-              {repeatWeekSelect}
-            </>
-          )}
-          <div>
-            {daysList.map((day: string, i: number) => (
-              <Checkbox
-                key={`${day}-${i}`}
-                checked={scheduleJob.repeats.selectedDays[day]}
-                onChange={e => setSelectedDays(e, day)}
-                value={day}
-                inputProps={{ 'aria-label': `${day}-checkbox` }}
+      <div className={classes.container}>
+        <InputLabel
+          htmlFor="calendar-team"
+          shrink={false}
+          className={classes.label}
+        >
+          Calendar
+        </InputLabel>
+        <SelectField
+          fullWidth
+          className="Scheduler__input Scheduler__team-input"
+          id="calendar-team"
+          placeholder="Select a team"
+          name="scheduleJob.calendarTeam"
+          options={mockCalendarTeams}
+        />
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container spacing={2}>
+            <Grid item xs>
+              <InputLabel
+                htmlFor="starts-on-date"
+                shrink={false}
+                className={classes.label}
+              >
+                Starts on
+              </InputLabel>
+              <KeyboardDatePicker
+                disableToolbar
+                className="Scheduler__input Scheduler__date-input"
+                format="MM/dd/yyyy"
+                id="starts-on-date"
+                value={scheduleJob.startsOn}
+                onChange={setDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date'
+                }}
               />
-            ))}
+            </Grid>
+            <Grid item xs>
+              <KeyboardTimePicker
+                className="Scheduler__input Scheduler__time-input"
+                id="starts-on-time"
+                value={scheduleJob.startsOn}
+                onChange={setDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time'
+                }}
+              />
+            </Grid>
+          </Grid>
+        </MuiPickersUtilsProvider>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            <InputLabel
+              htmlFor="repeats-num"
+              shrink={false}
+              className={classes.label}
+            >
+              Repeats every
+            </InputLabel>
+            <Grid container spacing={1}>
+              <Grid item xs>
+                <Field
+                  name="scheduleJob.repeats.num"
+                  className="Scheduler__input Scheduler__num-input"
+                  type="number"
+                  id="repeats-num"
+                  as={TextField}
+                />
+              </Grid>
+              <Grid item xs>
+                <SelectField
+                  className="Scheduler__input Scheduler__num-select"
+                  id="repeats-interval"
+                  name="scheduleJob.repeats.interval"
+                  options={mockRepeatsData}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs>
+            <InputLabel
+              htmlFor="fails-num"
+              shrink={false}
+              className={classes.label}
+            >
+              Fails after
+            </InputLabel>
+            <Grid container spacing={1}>
+              <Grid item xs>
+                <Field
+                  className="Scheduler__input Scheduler__num-input"
+                  name="scheduleJob.fails.num"
+                  type="number"
+                  id="fails-num"
+                  as={TextField}
+                />
+              </Grid>
+              <Grid item xs>
+                <SelectField
+                  className="Scheduler__input Scheduler__num-select"
+                  id="fails-interval"
+                  name="scheduleJob.fails.interval"
+                  options={mockFailsData}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        {isRepeatWeeksOrMonths && (
+          <div>
+            {isRepeatWeeks ? (
+              <FormHelperText>Repeat on</FormHelperText>
+            ) : (
+              <>
+                <FormHelperText>Repeat on the</FormHelperText>
+                {repeatWeekSelect}
+              </>
+            )}
+            <div>
+              {daysList.map((day: string, i: number) => (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      key={`${day}-${i}`}
+                      checked={scheduleJob.repeats.selectedDays[day]}
+                      onChange={e => setSelectedDays(e, day)}
+                      value={day}
+                      inputProps={{ 'aria-label': `${day}-checkbox` }}
+                    />
+                  }
+                  label={day}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={scheduleJob.enableJob}
-            onChange={e => setEnableJob(e)}
-            value="enableJob"
-            color="primary"
-          />
-        }
-        label="Enable this job"
-      />
-      <Grid container justify="flex-end" spacing={2}>
-        <Grid item>
-          <Button
-            onClick={() => setIsScheduleJobOpen(false)}
-            variant="contained"
-            color="secondary"
-          >
-            Cancel
-          </Button>
+        )}
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={scheduleJob.enableJob}
+              onChange={e => setEnableJob(e)}
+              value="enableJob"
+              color="primary"
+            />
+          }
+          label="Enable this job"
+        />
+        <Grid container justify="flex-end" spacing={2}>
+          <Grid item>
+            <Button
+              onClick={() => setIsScheduleJobOpen(false)}
+              variant="contained"
+              color="secondary"
+            >
+              Cancel
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary">
+              Submit Job
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary">
-            Submit Job
-          </Button>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   );
 };
 
