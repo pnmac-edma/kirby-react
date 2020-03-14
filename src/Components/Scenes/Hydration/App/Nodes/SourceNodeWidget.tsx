cb7dd3527be94e6e9eb293033bc67047d70a9119
Edward Roh <edwardroh89@gmail.com>
2020-03-14 09:38:35 -0700

    add node corner tag for source nodes

--- src/Components/Scenes/Hydration/App/Nodes/SourceNodeWidget.tsx
+++ src/Components/Scenes/Hydration/App/Nodes/SourceNodeWidget.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useFormikContext } from 'formik';
 import { makeStyles } from '@material-ui/core/styles';
 import color from '@edma/design-tokens/js/color';
 import { PortWidget } from '@projectstorm/react-diagrams';
+import {
+  InitialStateTypes,
+  NodeModel,
+  RDBMS,
+  API
+} from '../../../../../State/Hydration/types';
 
-const tileStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   tile: {
     borderColor: theme.palette.type === 'light' ? color.g600 : color.g500,
     backgroundColor: theme.palette.type === 'light' ? color.g600 : color.g500,
@@ -22,10 +29,26 @@ const tileStyles = makeStyles(theme => ({
   }
 }));
 
-const SourceNodeWidget = props => {
-  const { name, size } = props.node;
+interface SourceNodeWidgetProps {
+  node: NodeModel;
+}
+
+const SourceNodeWidget = ({ node }: SourceNodeWidgetProps) => {
+  const { name, id, size } = node;
+  const { values } = useFormikContext() as { values: InitialStateTypes };
+  const { sources } = values;
   const height = size * 0.5;
-  const classes = tileStyles();
+  const classes = useStyles();
+
+  const generateTag = (sourceType: string) => {
+    if (sourceType === 'RDBMS') {
+      return (sources[id] as RDBMS).sourceVersion;
+    }
+    if (sourceType === 'API') {
+      return (sources[id] as API).type;
+    }
+    return '';
+  };
 
   return (
     <div>
@@ -35,6 +58,7 @@ const SourceNodeWidget = props => {
             Source
           </div>
           <div className="Tile__source-name Tile__name">{name}</div>
+          <div>{generateTag(name)}</div>
         </div>
       </div>
       <div
@@ -44,7 +68,7 @@ const SourceNodeWidget = props => {
           left: size - 8
         }}
       >
-        <PortWidget name="right" node={props.node} />
+        <PortWidget name="right" node={node as any} />
       </div>
     </div>
   );
