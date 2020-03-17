67ac2227ae4183d5a08a5e791d6f10429e9ddc53
sfowles <sfowles@gmail.com>
2020-03-17 09:34:39 -0700

    Add input field for transform editor title

--- src/Components/Scenes/Hydration/App/Transform/TransformEditor.tsx
+++ src/Components/Scenes/Hydration/App/Transform/TransformEditor.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useFormikContext } from 'formik';
 import { ControlledEditor } from '@monaco-editor/react'; // https://github.com/suren-atoyan/monaco-react, similar to react-monaco-editor
 import {
   IconButton,
   makeStyles,
+  TextField,
   Tooltip,
   Typography,
   useTheme
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { color } from '@edma/design-tokens';
+import { color, z } from '@edma/design-tokens';
 import { setIsEditorOpen } from '../../../../../State/Hydration/actions';
 import { InitialStateTypes } from '../../../../../State/Hydration/types';
 import { keyboardShortcuts } from '../../../../../State/Hydration/helpers';
@@ -28,6 +29,43 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     top: '4.3rem',
     right: '0.5rem'
+  },
+  scriptName: {
+    margin: '1rem',
+    padding: 0,
+    verticalAlign: 'top',
+    display: 'block',
+    height: '24px',
+
+    '& .MuiInput-root:before, & .MuiInput-root:after': {
+      display: 'none'
+    },
+
+    '& .MuiInput-input': {
+      padding: 0
+    }
+  },
+  scriptNameBtn: {
+    transition: 'all .2s ease-in-out',
+    verticalAlign: 'top',
+    position: 'relative',
+    display: 'inline-block',
+    top: '-3px',
+    margin: '1rem',
+    height: '24px',
+
+    '&:hover': {
+      background: theme.palette.type === 'light' ? color.y50 : color.g600
+    },
+
+    '& ~ svg ': {
+      position: 'absolute',
+      height: '0.8em',
+      top: 19
+    }
+  },
+  untitledScriptName: {
+    background: theme.palette.type === 'light' ? color.y50 : color.g600
   }
 }));
 
@@ -45,6 +83,7 @@ const TransformEditor = (props: TransformProps) => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const theme = useTheme();
+  const [isScriptNameActive, setIsScriptNameActive] = useState(false);
 
   const setChange = (value: string | undefined) => {
     const newTransformsValue = {
@@ -63,9 +102,26 @@ const TransformEditor = (props: TransformProps) => {
       className={`Diagram__layer Editor ${classes.positioning}`}
     >
       <div className="Editor__heading" />
-      <Typography variant="body1" className={classes.name}>
-        {transforms[id].name}
-      </Typography>
+      {isScriptNameActive && (
+        <TextField
+          autoFocus
+          id="scriptName"
+          placeholder={transforms[id].name}
+          className={classes.scriptName}
+          onBlur={() => setIsScriptNameActive(!isScriptNameActive)}
+        />
+      )}
+      {!isScriptNameActive && (
+        <Typography
+          variant="body1"
+          className={`${classes.scriptNameBtn} ${
+            transforms[id].name === 'Untitled' ? classes.untitledScriptName : ''
+          }`}
+          onClick={() => setIsScriptNameActive(!isScriptNameActive)}
+        >
+          {transforms[id].name}
+        </Typography>
+      )}
       <Tooltip title="Close Editor">
         <IconButton
           className={classes.button}
