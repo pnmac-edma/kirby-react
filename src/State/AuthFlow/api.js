7e3637601d9c8435c17fa5e2a09264dd728dae1e
Edward Roh <edwardroh89@gmail.com>
2020-03-18 10:57:13 -0700

    configure authentication

--- src/State/AuthFlow/api.js
+++ src/State/AuthFlow/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { constructRequest } from '../helpers';
+import config from '../../config/config';
 
 // TODO: load from the env config once implemented
 // NOTE: authenicate still uses the older API. Do not change
-const BASE_URL = 'https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com/dev';
+// const BASE_URL = 'https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com/dev';
 
 export function authenticate(token) {
   const requestBody = {
@@ -11,11 +12,12 @@ export function authenticate(token) {
   };
 
   const request = constructRequest(
-    BASE_URL,
-    'POST',
-    '/authenticate',
-    null,
-    requestBody
+    config.authUrl, // url
+    config.authPath, // authenticationPath
+    'POST', // method
+    {
+      data: requestBody // the body of request
+    }
   );
 
   return axios(request)
--- src/config/config.js
+++ src/config/config.js
@@ -1,41 +1,64 @@
 const kirbyConfig = () => {
-  const baseApiUrl = 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com/';
-  const baseOldApiUrl =
-    'https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com/';
   const baseCogConUrl =
     'https://pennymac.onelogin.com/trust/saml2/http-post/sso/';
 
   const envSettings = {
     prd: {
-      hostname: 'kirby.pennymac.com',
-      apiUrl: `https://kirby-api.pnmac.com`,
+      // apiUrl: https://kirby-api.pnmac.com/(apiRoute)
+      // authUrl: https://kirby-api-sec.pnmac.com/authenticate
+      domain: 'kirby.pennymac.com',
+      apiUrl: 'https://kirby-api.pnmac.com',
+      apiBasePath: '/',
+      authUrl: 'https://kirby-api-sec.pnmac.com',
+      authPath: '/authenticate',
       cognitoUrl: `${baseCogConUrl}965273`,
       consoleUrl: `${baseCogConUrl}965275`
     },
     stg: {
-      hostname: 'd14zq0f3jiu0zb.cloudfront.net',
-      apiUrl: `https://kirby-api-stg.pnmac.com`,
+      // apiUrl: https://kirby-api.pnmac.com/(apiRoute)
+      // authUrl: https://kirby-api-stg-sec.pnmac.com/authenticate
+      // NOTE: I can't remember if sec came after stg or before
+      domain: 'd14zq0f3jiu0zb.cloudfront.net',
+      apiUrl: 'https://kirby-api-stg.pnmac.com',
+      apiBasePath: '/',
+      authUrl: 'https://kirby-api-stg-sec.pnmac.com',
+      authPath: '/authenticate',
       cognitoUrl: `${baseCogConUrl}964944`,
       consoleUrl: `${baseCogConUrl}965269`
     },
     dev: {
-      hostname: 'd1n1yc8ym1y64x.cloudfront.net',
+      // apiUrl: https://kirby-api.pnmac.com/dev/(apiRoute)
+      // authUrl: https://kirby-api-dev-sec.pnmac.com/authenticate
+      // NOTE: unsure of the url path for dev
+      domain: 'd1n1yc8ym1y64x.cloudfront.net',
+      apiBasePath: '/dev',
       apiUrl: 'https://kirby-api-dev.pnmac.com',
-      oldApiUrl: `${baseOldApiUrl}dev`,
+      authPath: '/dev/authenticate',
+      authUrl: 'https://kirby-api-dev-sec.pnmac.com',
       cognitoUrl: `${baseCogConUrl}877995`,
       consoleUrl: `${baseCogConUrl}877999`
     },
     ltr: {
-      hostname: 'kirby-website-litter.s3-website-us-west-2.amazonaws.com',
-      apiUrl: `${baseApiUrl}dev`,
-      oldApiUrl: `${baseOldApiUrl}dev`,
+      // apiUrl: https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com
+      // authUrl: apparently no auth url needed
+      // NOTE: unsure of j8nhpla4d3 or e6b8r4dht6 as url number
+      domain: 'kirby-website-litter.s3-website-us-west-2.amazonaws.com',
+      apiPath: '/dev',
+      apiUrl: 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com',
+      authPath: '/dev/authenticate',
+      authUrl: 'https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com',
       cognitoUrl: `${baseCogConUrl}877995`,
       consoleUrl: `${baseCogConUrl}877999`
     },
     offline: {
+      // apiUrl: https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com
+      // authUrl: apparently no auth url needed
+      // NOTE: unsure of j8nhpla4d3 or e6b8r4dht6 as url number
       hostname: 'localhost',
-      apiUrl: `${baseApiUrl}dev`,
-      oldApiUrl: `${baseOldApiUrl}dev`,
+      apiPath: '/dev',
+      apiUrl: 'https://j8nhpla4d3.execute-api.us-west-2.amazonaws.com',
+      authPath: '/dev/authenticate',
+      authUrl: 'https://e6b8r4dht6.execute-api.us-west-2.amazonaws.com',
       cognitoUrl: `${baseCogConUrl}877995`,
       consoleUrl: `${baseCogConUrl}877999`
     }
@@ -45,16 +68,16 @@ const kirbyConfig = () => {
   let env = null;
 
   switch (hostname) {
-    case envSettings.prd.hostname:
+    case envSettings.prd.domain:
       env = envSettings.prd;
       break;
     case envSettings.stg.url:
       env = envSettings.stg;
       break;
-    case envSettings.dev.hostname:
+    case envSettings.dev.domain:
       env = envSettings.dev;
       break;
-    case envSettings.ltr.hostname:
+    case envSettings.ltr.domain:
       env = envSettings.ltr;
       break;
     default:
