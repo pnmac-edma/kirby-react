0e00ba3871acaf5a67de55abbdd00c050b2842a5
Edward Roh <edwardroh89@gmail.com>
2020-03-23 10:03:26 -0700

    add custom link

--- src/Components/Scenes/Hydration/App/Nodes/LinkModel.js
+++ src/Components/Scenes/Hydration/App/Nodes/LinkModel.js
@@ -4,7 +4,7 @@ export default class LinkModel extends DefaultLinkModel {
   constructor() {
     super({
       type: 'standard',
-      width: 10
+      width: 2
     });
   }
 }
--- src/Components/Scenes/Hydration/App/Nodes/LinkSegment.js
+++ src/Components/Scenes/Hydration/App/Nodes/LinkSegment.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 const LinkSegment = ({ path }) => {
   return (
-    <path
-      fill="none"
-      // ref={ref => {
-      //   path = ref;
-      // }} // not sure what this is doing but ill keep for now
-      strokeWidth={15}
-      stroke="rgba(255,0,0,0.5)"
-      d={path}
-    />
+    <path fill="none" strokeWidth={8} stroke="rgba(255,0,0,0.5)" d={path} />
   );
 };
 
