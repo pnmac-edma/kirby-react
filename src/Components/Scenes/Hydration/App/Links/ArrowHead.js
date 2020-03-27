117717f91a619038be3d977290159834a600784f
Edward <edwardroh89@gmail.com>
2020-03-27 09:06:47 -0700

    added different arrow heads for sources vs transforms (#114)

--- src/Components/Scenes/Hydration/App/Links/ArrowHead.js
+++ src/Components/Scenes/Hydration/App/Links/ArrowHead.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export const markerHead = 'markerHead';
+export const sourceHead = 'sourceHead';
+export const transformHead = 'transformHead';
 
 const ArrowHead = () => {
   return (
     <svg>
       <defs>
         <marker
-          id={markerHead}
+          id={sourceHead}
           markerWidth="6"
           markerHeight="4"
           refX="3"
@@ -15,6 +16,16 @@ const ArrowHead = () => {
           orient="0deg"
         >
           <path d="M0,0 L0,4 L4,2 Z" style={{ fill: 'orange' }} />
+        </marker>
+        <marker
+          id={transformHead}
+          markerWidth="6"
+          markerHeight="4"
+          refX="3"
+          refY="2"
+          orient="0deg"
+        >
+          <path d="M0,0 L0,4 L4,2 Z" style={{ fill: 'pink' }} />
         </marker>
       </defs>
     </svg>
