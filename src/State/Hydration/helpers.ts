c424e3b0546957389dd4da68fe7eab4893dc62b2
Edward <edwardroh89@gmail.com>
2020-04-15 11:59:47 -0700

    fix delete bug (#144)

--- src/State/Hydration/helpers.ts
+++ src/State/Hydration/helpers.ts
@@ -6,7 +6,10 @@ export const keyboardShortcuts = {
     removeNodeFromDiagram: (node: NodeModel, subForm: any) => void,
     node: NodeModel
   ) => {
-    if (event.keyCode === 8 /* Backspace */) {
+    if (
+      event.keyCode === 8 /* Backspace */ ||
+      event.keyCode === 46 /* Delete */
+    ) {
       event.stopPropagation();
       if (node) {
         removeNodeFromDiagram(node, `${node.type}s`);
@@ -14,12 +17,18 @@ export const keyboardShortcuts = {
     }
   },
   source: (event: React.KeyboardEvent) => {
-    if (event.keyCode === 8 /* Backspace */) {
+    if (
+      event.keyCode === 8 /* Backspace */ ||
+      event.keyCode === 46 /* Delete */
+    ) {
       event.stopPropagation();
     }
   },
   codeEditor: (event: React.KeyboardEvent) => {
-    if (event.keyCode === 8 /* Backspace */) {
+    if (
+      event.keyCode === 8 /* Backspace */ ||
+      event.keyCode === 46 /* Delete */
+    ) {
       event.stopPropagation();
     }
   }
