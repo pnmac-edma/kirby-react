26eb4e3e05ce01ecbb92d808803f9440711e4a68
Edward <edwardroh89@gmail.com>
2020-04-22 12:36:21 -0700

    add approve request page for add destination (#150)

---
+++ src/Components/Scenes/ViewRequests/ApproveRequestAddDestination/ApproveRequestAddDestination.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Divider, Typography } from '@material-ui/core';
+import { WarningRounded } from '@material-ui/icons';
+import { color, font, fontSize } from '@edma/design-tokens';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    textTransform: 'none',
+    fontWeight: 'bold',
+    margin: theme.spacing(1.5)
+  },
+  flexStructure: {
+    display: 'flex',
+    justifyContent: 'left'
+  },
+  dividerStyle: {
+    width: '100%',
+    marginBottom: 32,
+    marginTop: 24
+  },
+  menuStyle: {
+    width: 'max-content',
+    textTransform: 'uppercase',
+    marginBottom: 8,
+    fontWeight: 'bold',
+    fontFamily: font.body,
+    color: theme.palette.type === 'light' ? color.black : color.g300
+  },
+  sidebar: {
+    background: theme.palette.type === 'light' ? color.g100 : color.g800,
+    fontSize: fontSize[1],
+    marginTop: '-10rem',
+    marginBottom: '-10rem',
+    overflow: 'hidden',
+    paddingTop: '10rem',
+    maxWidth: 430,
+    minWidth: 300,
+    width: '100%'
+  },
+  secondaryButton: {
+    color: color.b600,
+
+    '&:hover': {
+      color: color.b600,
+      background: color.b50
+    }
+  },
+  sideTable: {
+    width: '70%'
+  },
+  sideBarPostion: {
+    margin: '0 1rem',
+    color: theme.palette.type === 'light' ? color.black : color.white
+  },
+  title: {
+    width: 'max-content'
+  },
+  heading: {
+    margin: '1rem 0 2rem',
+    color: theme.palette.type === 'light' ? color.black : color.white
+  }
+}));
+
+const ApproveRequestAddDestination = ({
+  destinationRequest
+}: ApproveRequestAddDestinationProps) => {
+  const classes = useStyles();
+  const {
+    name,
+    description,
+    domain,
+    justification,
+    manager,
+    sensitivity
+  } = destinationRequest;
+
+  const sensitivityIcon = (sensitivity: string) => {
+    if (sensitivity === 'Sensitive') {
+      return (
+        <WarningRounded className="Tile__statusIcon Tile__statusIcon--warning" />
+      );
+    }
+    if (sensitivity === 'Confidential') {
+      return <WarningRounded color="error" />;
+    }
+    return null;
+  };
+
+  return (
+    <div className={classes.flexStructure}>
+      <div className={classes.sidebar}>
+        <div className={classes.sideBarPostion}>
+          <Typography variant="h2" className={classes.heading}>
+            Add Destination
+          </Typography>
+          <Typography variant="overline" className={classes.menuStyle}>
+            Destination Name
+          </Typography>
+          <Typography className={classes.title}>{name}</Typography>
+          <Divider className={classes.dividerStyle} />
+          <Typography variant="overline" className={classes.menuStyle}>
+            Sensitivity
+          </Typography>
+          <Typography className={classes.title}>
+            {sensitivityIcon(sensitivity)} {sensitivity}
+          </Typography>
+          <Divider className={classes.dividerStyle} />
+          <Typography variant="overline" className={classes.menuStyle}>
+            Domain
+          </Typography>
+          <Typography className={classes.title}>{domain}</Typography>
+          <Divider className={classes.dividerStyle} />
+          <Typography variant="overline" className={classes.menuStyle}>
+            Manager
+          </Typography>
+          <Typography className={classes.title}>{manager}</Typography>
+        </div>
+      </div>
+      <div className={classes.sideTable}>
+        <Typography variant="overline" className={classes.menuStyle}>
+          Description
+        </Typography>
+        <Typography className={classes.title}>{description}</Typography>
+        <Typography variant="overline" className={classes.menuStyle}>
+          Justification
+        </Typography>
+        <Typography className={classes.title}>{justification}</Typography>
+        <div>
+          <Button
+            className={`${classes.secondaryButton} ${classes.button}`}
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              // TODO: make call to api to mark in review request
+              // TODO: create snackbar notification on RequestsInbox page
+            }}
+          >
+            Mark In Review
+          </Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              // TODO: make call to api to approve request
+              // TODO: create snackbar notification on RequestsInbox page
+            }}
+          >
+            Approve
+          </Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              // TODO: make call to api to reject request
+              // TODO: create snackbar notification on RequestsInbox page
+            }}
+          >
+            Reject
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApproveRequestAddDestination;
+
+interface ApproveRequestAddDestinationProps {
+  destinationRequest: any; // TODO: add the shape of DestinationRequest
+}
