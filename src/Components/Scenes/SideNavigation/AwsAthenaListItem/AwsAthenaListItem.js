46471e256b82e45eff9666279b7aa75bf3544770
Selcuk Ates <selcuk.ates@pnmac.com>
2020-04-22 11:10:27 -0700

    its working need some code cleaning

--- src/Components/Scenes/SideNavigation/AwsAthenaListItem/AwsAthenaListItem.js
+++ src/Components/Scenes/SideNavigation/AwsAthenaListItem/AwsAthenaListItem.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
+import { useLocation, Link } from 'react-router-dom';
 import { makeStyles, ListItem, ListItemText } from '@material-ui/core';
 import { CloudQueue, ExitToApp } from '@material-ui/icons/';
 
@@ -13,9 +14,28 @@ const awsAthenaListItem = makeStyles(theme => ({
 
 const AwsAthenaListItem = () => {
   const classes = awsAthenaListItem();
+  const curPath = useLocation().pathname;
+  const [clicked, setClicked] = useState(false);
 
+  useEffect(() => {
+    if (clicked) {
+      // do something meaningful, Promises, if/else, whatever, and then
+      console.log('windown', window.location);
+      window.open(
+        'https://pennymac.onelogin.com/trust/saml2/http-post/sso/877999'
+      );
+    }
+  }, [clicked]);
+
+  console.log('Current path', curPath);
   return (
-    <ListItem button className="Nav__item">
+    <ListItem
+      button
+      onClick={() => setClicked(true)}
+      className="Nav__item"
+      component={Link}
+      to="/awsAthena"
+    >
       <CloudQueue className="Nav__icon" />
       <ListItemText className="Nav__text">
         AWS Athena
