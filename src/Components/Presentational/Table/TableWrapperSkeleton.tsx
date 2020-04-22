f4f790f51792fd74abaa490808cbf7fea3feba5a
Edward Roh <edwardroh89@gmail.com>
2020-04-22 14:42:29 -0700

    fix dom nesting error

--- src/Components/Presentational/Table/TableWrapperSkeleton.tsx
+++ src/Components/Presentational/Table/TableWrapperSkeleton.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { Skeleton } from '@material-ui/lab';
-import { TableRow, TableCell } from '@material-ui/core';
+import { TableRow, TableBody, TableCell } from '@material-ui/core';
 
 const TableWrapperSkeleton = () => {
   return (
-    <TableRow>
-      <TableCell>
-        <Skeleton
-          variant="rect"
-          width={600}
-          height={10}
-          style={{ margin: '1.5rem 1rem', borderRadius: 50 }}
-        />
-        <Skeleton
-          variant="rect"
-          width={600}
-          height={10}
-          style={{ margin: '2.5rem 1rem', borderRadius: 50 }}
-        />
-        <Skeleton
-          variant="rect"
-          width={600}
-          height={10}
-          style={{ margin: '2.5rem 1rem', borderRadius: 50 }}
-        />
-      </TableCell>
-    </TableRow>
+    <TableBody>
+      <TableRow>
+        <TableCell>
+          <Skeleton
+            variant="rect"
+            width={600}
+            height={10}
+            style={{ margin: '1.5rem 1rem', borderRadius: 50 }}
+          />
+          <Skeleton
+            variant="rect"
+            width={600}
+            height={10}
+            style={{ margin: '2.5rem 1rem', borderRadius: 50 }}
+          />
+          <Skeleton
+            variant="rect"
+            width={600}
+            height={10}
+            style={{ margin: '2.5rem 1rem', borderRadius: 50 }}
+          />
+        </TableCell>
+      </TableRow>
+    </TableBody>
   );
 };
 
